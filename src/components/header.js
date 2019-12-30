3c88a1d70b253808fd0771c25f93ce88ec44bdaa
Arsen <arsenohanyan@gmail.com>
2019-12-30 09:44:55 -0800

    Navbar Sceleton Ready

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,60 @@
-import { Link } from "gatsby"
+//import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import React,{useState, useLayoutEffect} from "react"
+//useEffect
+import "./header.scss";
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
+const Header = ({ siteTitle }) => {  
+  const [isOnTop, setIsOnTop] = useState(true);
+
+  useScrollPosition(( currPos ) => {
+    if (currPos.y !== 0) setIsOnTop(false);
+    else setIsOnTop(true);
+  })
+  
+  return <header className={!isOnTop ? 'active' : ''}>
     <div
       style={{
         margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        display: 'flex',
+        listStyle: 'none',
+        padding: `1rem`,
+        justifyContent: 'space-between',
+
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+      <div></div>
+      <div></div>
     </div>
   </header>
-)
+}
+
+function getScrollPosition({ element, useWindow }) {
+  const isBrowser = typeof window !== `undefined`
+  if (!isBrowser) return { x: 0, y: 0 }
+
+  const target = element ? element.current : document.body
+  const position = target.getBoundingClientRect()
+
+  return useWindow
+    ? { x: window.scrollX, y: window.scrollY }
+    : { x: position.left, y: position.top }
+}
+
+function useScrollPosition(effect, deps, element, useWindow, wait) {
+
+
+  const callBack = () => {
+    const currPos = getScrollPosition({ element, useWindow })
+    effect(currPos)
+  }
+  useLayoutEffect(() => {
+
+    window.addEventListener('scroll', callBack)
+
+    return () => window.removeEventListener('scroll', callBack)
+  })
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
